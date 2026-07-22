500cb0afbb852ddedb3d476a370fe581038394dd
MMiter <mitermi2022@163.com>
2026-07-22 13:54:32 +0800

    update dataset

--- WaterQuality/RawData/DataProcessing.py
+++ WaterQuality/RawData/DataProcessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 读取 CSV，将 '--' 识别为缺失值
-df = pd.read_csv('./RawData/2026-07-22-0800.csv', na_values=['--'])
+df = pd.read_csv('./RawData/2026-07-22-1200.csv', na_values=['--'])
 
 # 删除指定列（注意列名与文件完全一致，若有空格需保留）
 cols_to_drop = ['省份', '流域', '断面名称', '监测时间', '叶绿素α(mg/L)', '藻密度(cells/L)']
@@ -13,4 +13,4 @@ df_clean = df_dropped.dropna(how='any')
 # 查看结果
 print(df_clean)
 
-df_clean.to_csv('./Data/2026-07-22-0800-Cleaned.csv', index=False)
+df_clean.to_csv('./Data/2026-07-22-1200-Cleaned.csv', index=False)
